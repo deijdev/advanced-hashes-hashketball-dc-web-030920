174b0bbd96639fd82d304e12ecaad325947ccf09
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 15:52:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,6 +121,7 @@ end
 
 def num_points_scored(player_x)
   game_hash.each do |home_away, keys|
+    binding.pry
     keys[:players].each do |player|
       return player[:points] if player[:player_name] == player_x
     end
